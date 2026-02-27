48d2c146a69769bd594f83041231ff93e5ddca24
bycov <bycov1976@gmail.com>
2026-02-27 07:44:18 +0500

    Update QMG_MCMC.py

--- QMG_MCMC.py
+++ QMG_MCMC.py
@@ -1,3 +1,17 @@
+# Copyright 2026 Bykov Denis Alexandrovich (bycov)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Часть 4: Класс QMG (без изменений)
 import numpy as np
 import pandas as pd
@@ -214,5 +228,6 @@ plt.grid(True, alpha=0.3)
 
 plt.tight_layout()
 plt.show()
+
 
 print(f"S₈ среднее = {np.mean(s8_samples):.3f} ± {np.std(s8_samples):.3f}")
